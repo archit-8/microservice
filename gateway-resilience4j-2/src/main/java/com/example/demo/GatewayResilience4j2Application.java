2c097513e28252aacc946681a79d3581deae7851
Archit Singh Chauhan <88979004+archit-8@users.noreply.github.com>
2025-12-15 09:49:57 +0000

    Add files via upload

---
+++ gateway-resilience4j-2/src/main/java/com/example/demo/GatewayResilience4j2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayResilience4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayResilience4j2Application.class, args);
+	}
+
+}
